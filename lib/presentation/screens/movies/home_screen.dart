b1af0d881167b16f216e40fc3e82f630a7b29ad8
josemlegal <josemlegal97@gmail.com>
2023-05-28 15:44:34 -0400

    Change pc

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,4 +1,3 @@
-import 'package:cinemapedia/core/constants/environment.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -7,7 +6,7 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: Center(
         child: Placeholder(),
       ),
