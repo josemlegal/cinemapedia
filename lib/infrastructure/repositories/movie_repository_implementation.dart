59eb7cbeaf46bc5a1bafa4ef2910028ba8b914a5
josemlegal <josemlegal97@gmail.com>
2023-07-15 17:16:00 -0400

    Add movie repository implementation

---
+++ lib/infrastructure/repositories/movie_repository_implementation.dart
@@ -0,0 +1,14 @@
+import 'package:cinemapedia/domain/datasources/movies_datasource.dart';
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/domain/repositories/movies_repository.dart';
+
+class MovieRepositoryImplementation extends MoviesRepository {
+  final MoviesDataSource dataSource;
+
+  MovieRepositoryImplementation(this.dataSource);
+
+  @override
+  Future<List<Movie>> getNowPlaying({int page = 1}) {
+    return dataSource.getNowPlaying(page: page);
+  }
+}
