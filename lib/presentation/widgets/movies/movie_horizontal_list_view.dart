a444440be4570a938c686649d7bdb602e71de4e4
josemlegal <josemlegal97@gmail.com>
2023-07-16 12:50:06 -0400

    Add movie details route and get the movie id to navigate

--- lib/presentation/widgets/movies/movie_horizontal_list_view.dart
+++ lib/presentation/widgets/movies/movie_horizontal_list_view.dart
@@ -2,6 +2,7 @@ import 'package:animate_do/animate_do.dart';
 import 'package:cinemapedia/core/helpers/human_formats.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class MovieHorizontalListView extends StatefulWidget {
   final List<Movie> movies;
@@ -54,14 +55,18 @@ class _MovieHorizontalListViewState extends State<MovieHorizontalListView> {
             _Title(title: widget.title, subTitle: widget.subTitle),
           Expanded(
             child: ListView.builder(
-                controller: scrollController,
-                scrollDirection: Axis.horizontal,
-                itemCount: widget.movies.length,
-                physics: const BouncingScrollPhysics(),
-                itemBuilder: (context, index) {
-                  return FadeInRight(
-                      child: _Slide(movie: widget.movies[index]));
-                }),
+              controller: scrollController,
+              scrollDirection: Axis.horizontal,
+              itemCount: widget.movies.length,
+              physics: const BouncingScrollPhysics(),
+              itemBuilder: (context, index) {
+                return FadeInRight(
+                  child: _Slide(
+                    movie: widget.movies[index],
+                  ),
+                );
+              },
+            ),
           ),
         ],
       ),
@@ -105,7 +110,10 @@ class _Slide extends StatelessWidget {
                     );
                   }
 
-                  return FadeIn(child: child);
+                  return GestureDetector(
+                    onTap: () => context.push('/movie/${movie.id}'),
+                    child: FadeIn(child: child),
+                  );
                 },
               ),
             ),
