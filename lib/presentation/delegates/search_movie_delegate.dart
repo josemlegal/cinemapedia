80151aca4c2c0d4d7c9d8814e34b304be7d75a6e
josemlegal <josemlegal97@gmail.com>
2023-07-18 19:53:20 -0400

    Add vote average to the search results and add decimals to the human formats function

--- lib/presentation/delegates/search_movie_delegate.dart
+++ lib/presentation/delegates/search_movie_delegate.dart
@@ -1,4 +1,5 @@
 import 'package:animate_do/animate_do.dart';
+import 'package:cinemapedia/core/helpers/human_formats.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:flutter/material.dart';
 
@@ -72,7 +73,7 @@ class _MovieItem extends StatelessWidget {
           SizedBox(
             width: size.width * 0.2,
             child: ClipRRect(
-              borderRadius: BorderRadius.circular(20.0),
+              borderRadius: BorderRadius.circular(10.0),
               child: Image.network(
                 movie.posterPath,
                 loadingBuilder: (context, child, loadingProgress) =>
@@ -90,6 +91,18 @@ class _MovieItem extends StatelessWidget {
                 (movie.overview.length > 100)
                     ? Text('${movie.overview.substring(0, 100)}...')
                     : Text(movie.overview),
+                Row(
+                  children: [
+                    Icon(Icons.star_half_rounded,
+                        color: Colors.yellow.shade800),
+                    const SizedBox(width: 5.0),
+                    Text(
+                      HumanFormats.number(movie.voteAverage, 1),
+                      style: textStyle.bodyMedium!
+                          .copyWith(color: Colors.yellow.shade900),
+                    )
+                  ],
+                )
               ],
             ),
           )
