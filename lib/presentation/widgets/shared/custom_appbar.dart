943d3d59d813ba65f5a93c34760ee74ab49c8890
josemlegal <josemlegal97@gmail.com>
2023-07-18 22:23:05 -0400

    Persist the latest query search

--- lib/presentation/widgets/shared/custom_appbar.dart
+++ lib/presentation/widgets/shared/custom_appbar.dart
@@ -30,10 +30,19 @@ class CustomAppBar extends ConsumerWidget {
               icon: const Icon(Icons.search),
               onPressed: () {
                 final movieRepository = ref.read(movieRepositoryProvider);
+                final searchQuery = ref.read(searchQueryProvider);
+
                 showSearch<Movie?>(
+                  query: searchQuery,
                   context: context,
                   delegate: SearchMovieDelegate(
-                      searchMovies: movieRepository.searchMovies),
+                    searchMovies: (query) {
+                      ref
+                          .read(searchQueryProvider.notifier)
+                          .update((state) => query);
+                      return movieRepository.searchMovies(query);
+                    },
+                  ),
                 ).then((movie) {
                   if (movie == null) return;
                   context.push('/movie/${movie.id}');
