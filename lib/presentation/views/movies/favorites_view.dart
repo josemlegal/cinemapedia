3a12226ea90e88f3dccc2903d028889caa4d220d
josemlegal <josemlegal97@gmail.com>
2023-08-04 11:14:05 -0400

    Add masonry

--- lib/presentation/views/movies/favorites_view.dart
+++ lib/presentation/views/movies/favorites_view.dart
@@ -1,16 +1,28 @@
+import 'package:cinemapedia/presentation/providers/providers.dart';
+import 'package:cinemapedia/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class FavoritesView extends StatelessWidget {
+class FavoritesView extends ConsumerStatefulWidget {
   const FavoritesView({super.key});
 
   @override
+  FavoritesViewState createState() => FavoritesViewState();
+}
+
+class FavoritesViewState extends ConsumerState<FavoritesView> {
+  @override
+  void initState() {
+    super.initState();
+    ref.read(favoritesMoviesProvider.notifier).loadNextPage();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final favoritesMovies = ref.watch(favoritesMoviesProvider).values.toList();
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Favorites'),
-      ),
-      body: const Center(
-        child: Text('Favorites'),
+      body: MovieMasonry(
+        movies: favoritesMovies,
       ),
     );
   }
--- lib/presentation/widgets/widgets.dart
+++ lib/presentation/widgets/widgets.dart
@@ -3,3 +3,5 @@ export 'movies/movies_slideshow.dart';
 export 'shared/custom_bottom_navigation_bar.dart';
 export 'movies/movie_horizontal_list_view.dart';
 export 'shared/full_screen_loader.dart';
+export 'movies/movie_masonry.dart';
+export 'movies/movie_poster_link.dart';
